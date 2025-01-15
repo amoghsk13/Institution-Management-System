8e8baac9c8e962da1df7c3e3bc016e6b5386c223
amoghsk13 <155971870+amoghsk13@users.noreply.github.com>
2025-01-16 02:42:36 +0530

    Delete student-service directory

--- student-service/student-service/src/main/java/com/codeelevate/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeelevate.repository;
-
-import com.codeelevate.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}

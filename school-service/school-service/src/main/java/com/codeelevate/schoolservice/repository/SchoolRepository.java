42455f0d1c37af53f82671705b3f0f506b00199e
amoghsk13 <155971870+amoghsk13@users.noreply.github.com>
2025-01-16 02:42:29 +0530

    Delete school-service directory

--- school-service/school-service/src/main/java/com/codeelevate/schoolservice/repository/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeelevate.schoolservice.repository;
-
-import com.codeelevate.schoolservice.entity.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SchoolRepository extends JpaRepository<School, Long> {
-}
